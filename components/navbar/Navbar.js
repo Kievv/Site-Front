877af62431e6afc211af61cf3a9d21ae79a89255
Ranom Morais Ruiz <ranom.ruiz@grupoicts.com.br>
2023-06-27 21:38:46 -0300

    Atualizando para nextjs

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -2,26 +2,39 @@ import Link from 'next/link'
 import styles from '@styles/navbar.module.css'
 
 export default function Navbar() {
-  return (
-    <div className={styles.container}>
-        <nav className={styles.nav}>
-            <Link className={styles.links} href="/">REMEDIE</Link>
-            <ul className={styles.ul}>           
-                <li className={styles.li}>
-                    <Link href="/">Home</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link href="/consultas">Consultas</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link href="/exames">Exames</Link>
-                </li>
-                <li className={styles.li}>
-                    <Link href="/remedios">Remédios</Link>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    
-  )
+    return (
+        
+
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <Link className={styles.links} href="/">REMEDIE</Link>
+                <ul className={styles.ul}>
+                    <div className={styles.botaoNavbar}>
+                        <li className={styles.li}>
+                            <Link href="/">Home</Link>
+                        </li>
+                    </div>
+
+                    <div className={styles.botaoNavbar}>
+                        <li className={styles.li}>
+                            <Link href="/consultas">Consultas</Link>
+                        </li>
+                    </div>
+
+                    <div className={styles.botaoNavbar}>
+                        <li className={styles.li}>
+                            <Link href="/exames">Exames</Link>
+                        </li>
+                    </div>
+
+                    <div className={styles.botaoNavbar}>
+                        <li className={styles.li}>
+                            <Link href="/remedios">Remédios</Link>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+
+    )
 }
